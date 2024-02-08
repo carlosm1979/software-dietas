d347b381d2f5789a610311400d67a006f1f36b6f
carlosm1979 <carlosm1979@gmail.com>
2024-02-08 02:04:10 +0100

    Add-rotuer (#19)
    Co-authored-by: cmgvdietas <155473180+cmgvdietas@users.noreply.github.com>

--- webapp/src/App.cy.tsx
+++
@@ -1,7 +0,0 @@
-import App from "./App"
-
-it('stepper should default to 0', () => {
-    cy.mount(<App />)
-    cy.get('p').should('have.text', 'Edit src/App.tsx and save to reload.')
-    cy.screenshot()
-  })
