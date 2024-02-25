01e288eb0b2e8a4644ba2d4c10788c92fb5c5835
cmgvdietas <155473180+cmgvdietas@users.noreply.github.com>
2024-02-26 00:34:44 +0100

    Develop (#22)

--- webapp/src/Layout /index.tsx
+++ webapp/src/Layout /index.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom"
-
+import { Link } from "react-router-dom";
 export const Layout = () => {
 
 
     return (<div>
         <h1>Software dietas</h1>
+        <Link to={'/search'}>Search</Link>
         <Outlet />
     </div>)
 }
